00033d0a940d1d3da9c6f97d552e8298a7072f28
opacarophile08 <2021ugcs112@nitjsr.ac.in>
2024-07-02 16:40:02 +0530

    final commit

--- frontend/src/components/Authentication/Signup.js
+++ frontend/src/components/Authentication/Signup.js
@@ -101,8 +101,8 @@ const Signup = () => {
       const data = new FormData();
       data.append("file", pics);
       data.append("upload_preset", "chat-app");
-      data.append("cloud_name", "piyushproj");
-      fetch("https://api.cloudinary.com/v1_1/piyushproj/image/upload", {
+      data.append("cloud_name", "dcuedo1bg");
+      fetch("https://api.cloudinary.com/v1_1/dcuedo1bg/image/upload", {
         method: "post",
         body: data,
       })
